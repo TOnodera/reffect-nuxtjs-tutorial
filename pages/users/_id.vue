47dfdd0bc0fd4bbd05734fa57848dd8d2ad4403e
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-03 09:36:33 +0000

    パラメータが一つの場合の渡し方を確認

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1>ユーザーID: {{ this.$route.params.id }}</h1>
+    <h1>ユーザーID: {{ user.name }}</h1>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            id: this.$route.params.id,
+            users: [
+                {
+                    id: 1,
+                    name: 'John Doe',
+                },
+                {
+                    id: 2,
+                    name: 'Kevin Smith',
+                },
+                {
+                    id: 3,
+                    name: 'Harry Bosch',
+                },
+            ],
+        };
+    },
+    computed: {
+        user() {
+            return this.users.find((user) => user.id == this.id);
+        },
+    },
+};
+</script>
