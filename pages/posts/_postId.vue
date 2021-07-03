4a6b19df094986b3cf51f7f6184a4a6984e19c97
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-03 11:25:00 +0000

    asyncData使用しないで実装

---
+++ pages/posts/_postId.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>記事個別ページ</h1>
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.body }}</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    head() {
+        return {
+            title: this.post.title,
+        };
+    },
+    data() {
+        return {
+            post: {},
+        };
+    },
+    async mounted() {
+        const response = await axios.get(
+            `https://jsonplaceholder.typicode.com/posts/${this.$route.params.postId}`
+        );
+        this.post = response.data;
+    },
+};
+</script>
