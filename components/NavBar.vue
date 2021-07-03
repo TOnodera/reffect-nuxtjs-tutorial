66e7b5306516405d6ce1de70c56ba0564c0d4171
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-03 09:29:09 +0000

    動的ページの作成の仕方

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,5 +2,6 @@
     <nav>
         <nuxt-link to="/">Home</nuxt-link>
         <nuxt-link to="/about">About</nuxt-link>
+        <nuxt-link :to="{ name: 'users-list' }">User List</nuxt-link>
     </nav>
 </template>
