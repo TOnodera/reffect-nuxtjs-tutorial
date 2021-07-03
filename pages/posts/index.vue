612e8ec35324e73ffc9d1e11a2288b11caaed8f1
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-03 11:10:08 +0000

    posts一覧作成

---
+++ pages/posts/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>記事一覧</h1>
+        <ul>
+            <li v-for="post in posts" :key="post.id">
+                {{ post.title }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            posts: [],
+        };
+    },
+    async mounted() {
+        const response = await axios.get(
+            'https://jsonplaceholder.typicode.com/posts'
+        );
+        this.posts = response.data;
+    },
+};
+</script>
