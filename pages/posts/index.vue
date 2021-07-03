b37a05428a52ed63af02cbb6e582f48c2aef61ec
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-03 11:50:23 +0000

    ローディングバー表示されるようになった

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -3,7 +3,9 @@
         <h1>記事一覧</h1>
         <ul>
             <li v-for="post in posts" :key="post.id">
-                {{ post.title }}
+                <nuxt-link :to="'/posts/' + post.id">{{
+                    post.title
+                }}</nuxt-link>
             </li>
         </ul>
     </div>
